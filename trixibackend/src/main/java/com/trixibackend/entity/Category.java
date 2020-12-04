a85aff4348129e452daae915f7ad00a8c87c8e11
alebal14 <alexandra.b.balogh@gmail.com>
2020-12-04 11:01:27 +0100

    entities

---
+++ trixibackend/src/main/java/com/trixibackend/entity/Category.java
@@ -0,0 +1,38 @@
+package com.trixibackend.entity;
+
+import org.bson.types.ObjectId;
+
+public class Category {
+    private ObjectId id;
+    private String name;
+
+    public  Category(){}
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ trixibackend/src/main/java/com/trixibackend/entity/Comment.java
@@ -0,0 +1,59 @@
+package com.trixibackend.entity;
+
+import org.bson.types.ObjectId;
+
+public class Comment {
+
+    private ObjectId id;
+    private String comment;
+    private String postId;
+    private String userId;
+
+    public Comment(){}
+
+    public Comment(String comment) {
+        this.comment = comment;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", comment='" + comment + '\'' +
+                ", postId='" + postId + '\'' +
+                ", userId='" + userId + '\'' +
+                '}';
+    }
+}
