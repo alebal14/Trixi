ea8ad5951181aa7bbea18ee0fea92cfe00b38e6b
anna <zoora1234@hotmail.com>
2020-12-09 22:22:33 +0100

    cookiesSession working

--- trixibackend/src/main/java/com/trixibackend/RestApi.java
+++ trixibackend/src/main/java/com/trixibackend/RestApi.java
@@ -4,6 +4,7 @@ import com.trixibackend.entity.*;
 import express.Express;
 import express.http.Cookie;
 import express.http.SessionCookie;
+import express.middleware.Middleware;
 
 import java.util.Map;
 
@@ -18,6 +19,8 @@ public class RestApi {
 
     private void initApi() {
         app.listen(3000);
+        // sets a unique cookie on each client to track authentication
+        app.use(Middleware.cookieSession("f3v4", 60 * 60 * 24 * 7));
         System.out.println("server started on port 3000");
 
         var allCollectionsName = db.getDatabase().listCollectionNames();
@@ -31,6 +34,7 @@ public class RestApi {
         setUpUpdateApi();
         setLoginUser();
         getLoggedinUser();
+        logoutUser();
 
 
     }
@@ -171,12 +175,12 @@ public class RestApi {
 
         app.post("/rest/login", (req, res) ->{
 
-           /* var sessionCookie = (SessionCookie) req.getMiddlewareContent("sessioncookie");
+            var sessionCookie = (SessionCookie) req.getMiddlewareContent("sessioncookie");
 
             if(sessionCookie.getData() != null) {
                 res.send("Already logged in");
                 return;
-            }*/
+            }
 
             User loggedInUser = (User) req.getBody(User.class);
             User user = (User) db.getLoginByNameOrEmail(loggedInUser);
@@ -191,7 +195,7 @@ public class RestApi {
                 return;
             }
 
-           // sessionCookie.setData(user);
+            sessionCookie.setData(user);
             user.setPassword(null); // sanitize password
             res.json(user);
         });
@@ -212,6 +216,9 @@ public class RestApi {
 
         });
 
+    }
+
+    private void logoutUser(){
         app.get("/rest/logout", (req, res) -> {
             var sessionCookie = (SessionCookie) req.getMiddlewareContent("sessioncookie");
             sessionCookie.setData(null);
