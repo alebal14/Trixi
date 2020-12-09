7b4289b30ec6a0c4dadf0601ba486013a0214bde
Sofia <sofia.fredmans@gmail.com>
2020-12-09 14:06:56 +0100

    recyclerview added

--- app/src/main/java/com/example/trixi/ui/home/HomepageActivity.kt
+++ app/src/main/java/com/example/trixi/ui/home/HomepageActivity.kt
@@ -1,4 +1,39 @@
 package com.example.trixi.ui.home
 
-class HomepageActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.trixi.R
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.activity_home.*
+
+
+class HomepageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val adapter = GroupAdapter<GroupieViewHolder> ()
+
+        adapter.add(HomeItem())
+        adapter.add(HomeItem())
+        adapter.add(HomeItem())
+        adapter.add(HomeItem())
+
+        recyclerView_homepage.adapter = adapter;
+    }
 }
+
+    class HomeItem : Item<GroupieViewHolder> (){
+        override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+
+        }
+
+
+    override fun getLayout(): Int {
+        return R.layout.home_item;
+    }
+
+
+    }
