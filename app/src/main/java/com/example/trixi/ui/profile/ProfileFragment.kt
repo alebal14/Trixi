3a99e64538ea3c84f5aa616b64e2b5842d1a40a7
Michelle Opey <mipanowicz@gmail.com>
2020-12-14 15:25:20 +0100

    wip recycleview

--- app/src/main/java/com/example/trixi/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/trixi/ui/profile/ProfileFragment.kt
@@ -1,21 +1,23 @@
 package com.example.trixi.ui.profile
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.trixi.R
+import com.example.trixi.apiService.RetrofitClient
+import com.example.trixi.entities.Post
 import com.example.trixi.entities.User
 import com.example.trixi.repository.PostToDb
 import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
-import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
-import com.xwray.groupie.kotlinandroidextensions.Item
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.fragment_home_item.view.*
 import kotlinx.android.synthetic.main.fragment_profile.*
-import kotlinx.android.synthetic.main.profile_media_item.*
 import kotlinx.android.synthetic.main.profile_media_item.view.*
 
 class ProfileFragment : Fragment() {
@@ -34,6 +36,11 @@ class ProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         populateProfile()
         populateMediaGrid()
+        populatePetList()
+    }
+
+    private fun populatePetList() {
+        TODO("Populate list with users pets")
     }
 
     private fun populateProfile() {
@@ -41,8 +48,7 @@ class ProfileFragment : Fragment() {
         if(loggedInUser != null) {
             profile_name.text = loggedInUser.userName
             profile_bio.text = loggedInUser.bio
-            //use your ip here
-            Picasso.get().load("http://192.168.8.101:3000/" + loggedInUser.imageUrl).fit().into(profile_image)
+            Picasso.get().load(RetrofitClient.BASE_URL + loggedInUser.imageUrl).fit().into(profile_image)
             owner_name.visibility = View.INVISIBLE
 
             //TODO: get followers & following
@@ -53,27 +59,23 @@ class ProfileFragment : Fragment() {
 
     private fun populateMediaGrid() {
 
-        val adapter = GroupAdapter<GroupieViewHolder>()
-        media_grid.layoutManager = GridLayoutManager(context, 4)
-
-        adapter.add(MediaItem())
-        adapter.add(MediaItem())
-        adapter.add(MediaItem())
-        adapter.add(MediaItem())
-
-        media_grid.adapter = adapter
-
-    }
-
-    class MediaItem() : Item() {
-        //TODO make it post
-        override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-            viewHolder.itemView.apply {
-                Picasso.get().load("https://imgur.com/IjMSpbA").into(media_item_thumbnail)
-            }
+        val mediaAdapter = GroupAdapter<GroupieViewHolder>().apply {
+            spanCount = 3
         }
-        override fun getLayout() : Int = R.layout.profile_media_item
+
+        media_grid.apply {
+            layoutManager = GridLayoutManager(context, mediaAdapter.spanCount).apply {
+                spanSizeLookup = mediaAdapter.spanSizeLookup
+            }
+            adapter = mediaAdapter
+        }
+
+        mediaAdapter.add(MediaItem())
+        mediaAdapter.add(MediaItem())
+
     }
+
+
 
     private fun fetchUserPosts() {
         //TODO: fetch the posts for the user and show in grid,
@@ -86,6 +88,17 @@ class ProfileFragment : Fragment() {
 
 
 }
+
+class MediaItem() : Item<GroupieViewHolder>() {
+    //TODO make it post
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        viewHolder.itemView.apply {
+            Picasso.get().load("https://imgur.com/IjMSpbA").into(media_item_thumbnail)
+        }
+    }
+    override fun getLayout() : Int = R.layout.profile_media_item
+}
+
 
 
 
--- app/src/main/java/com/example/trixi/ui/profile/ProfileViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.trixi.ui.profile
-
-import com.example.trixi.repository.GetFromDbViewModel
-import java.io.Serializable
-
-class ProfileViewModel(profileId: Any, dataSource: GetFromDbViewModel) : Serializable {
-}
