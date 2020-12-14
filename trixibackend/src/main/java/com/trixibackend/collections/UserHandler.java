961ee869ebb32e9ea69ff9d99a0dc59e41d73758
alebal14 <alexandra.b.balogh@gmail.com>
2020-12-14 23:04:07 +0100

    new api get latest post

--- trixibackend/src/main/java/com/trixibackend/collections/UserHandler.java
+++ trixibackend/src/main/java/com/trixibackend/collections/UserHandler.java
@@ -120,6 +120,13 @@ public class UserHandler {
         return listOutput;
     }
 
+    public Post userLatestPost(User user){
+        List<Post> getAllthePost = user.getPosts();
+        Post last = getAllthePost.stream().reduce((first, second) -> second)
+                .orElse(null);
+        return last;
+    }
+
 
     public User findUserByNameOrEmail(User loggedInUser) {
         String emailOrUsername;
