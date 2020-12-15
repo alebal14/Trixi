c2e0830f0917d0f024965515ed0bb995844e3b6a
Michelle Opey <mipanowicz@gmail.com>
2020-12-15 10:43:01 +0100

    profile mvp?

--- trixibackend/src/main/java/com/trixibackend/RestApi.java
+++ trixibackend/src/main/java/com/trixibackend/RestApi.java
@@ -9,23 +9,11 @@ import express.http.Cookie;
 import express.http.SessionCookie;
 import express.middleware.Middleware;
 import express.utils.Status;
-import express.utils.Status;
 import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.MultipartStream;
-import org.apache.commons.io.FilenameUtils;
-
-import javax.naming.Reference;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class RestApi {
 
