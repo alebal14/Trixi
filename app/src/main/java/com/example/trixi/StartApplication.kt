6670462aeb963881b13d828ea3319eb338604dcf
AnnaKarlsson3 <zoora1234@hotmail.com>
2020-12-16 17:21:10 +0100

    wip realm

--- app/src/main/java/com/example/trixi/StartApplication.kt
+++ app/src/main/java/com/example/trixi/StartApplication.kt
@@ -2,14 +2,23 @@ package com.example.trixi
 
 import android.app.Application
 import com.example.trixi.apiService.RetrofitClient
+import io.realm.Realm
+import io.realm.RealmConfiguration
 
 class StartApplication: Application() {
 
 
     override fun onCreate() {
         super.onCreate()
-
      RetrofitClient.context = this
+
+      Realm.init(this)
+        val configuration = RealmConfiguration.Builder()
+            .name("comicDb")
+            .schemaVersion(1)
+            .deleteRealmIfMigrationNeeded()
+            .build()
+        Realm.setDefaultConfiguration(configuration)
     }
 
 }
