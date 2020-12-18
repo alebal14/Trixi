f213509781c3ee915b264096da62c2db17666e64
Sofia <sofia.fredmans@gmail.com>
2020-12-18 10:44:50 +0100

    fix

--- app/src/main/java/com/example/trixi/MainActivity.kt
+++ app/src/main/java/com/example/trixi/MainActivity.kt
@@ -7,14 +7,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.trixi.repository.DataViewModel
 import com.example.trixi.repository.PostToDb
-import com.example.trixi.ui.fragments.SearchFragment
+import com.example.trixi.ui.discover.ShowTopPostsFragment
 import com.example.trixi.ui.fragments.UploadFragment
 import com.example.trixi.ui.fragments.singlePostFragment
 import com.example.trixi.ui.home.HomepageFragment
 import com.example.trixi.ui.profile.LoggedInUserProfileFragment
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.MainScope
 
 
 class MainActivity : AppCompatActivity() {
@@ -33,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 
          val homepageFragment = HomepageFragment()
          val postFragment = UploadFragment()
-         val searchFragment = SearchFragment()
+         val discoverFragment = ShowTopPostsFragment()
          val profileFragment = LoggedInUserProfileFragment()
          val singleFragment = singlePostFragment()
 
@@ -71,7 +69,7 @@ class MainActivity : AppCompatActivity() {
          bottom_nav.setOnNavigationItemSelectedListener {
              when(it.itemId){
                  R.id.footer_home -> makeCurrentFragment(homepageFragment)
-                 R.id.footer_search -> makeCurrentFragment(searchFragment)
+                 R.id.footer_search -> makeCurrentFragment(discoverFragment)
                  R.id.footer_post -> makeCurrentFragment(postFragment)
                  R.id.footer_profile -> makeCurrentFragment(profileFragment)
              }
