c7b4352d48fab707e7453e52699e5c96853974d7
Michelle Opey <mipanowicz@gmail.com>
2021-01-04 13:23:12 +0100

    populated user profile

--- app/src/main/java/com/example/trixi/ui/profile/UserProfileFragment.kt
+++ app/src/main/java/com/example/trixi/ui/profile/UserProfileFragment.kt
@@ -3,13 +3,25 @@ package com.example.trixi.ui.profile
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.SnapHelper
+import com.example.marvelisimo.adapter.ProfileMediaGridAdapter
 import com.example.trixi.R
+import com.example.trixi.apiService.RetrofitClient.Companion.BASE_URL
+import com.example.trixi.entities.Pet
+import com.example.trixi.entities.Post
 import com.example.trixi.entities.User
 import com.example.trixi.repository.TrixiViewModel
+import com.github.rubensousa.gravitysnaphelper.GravitySnapHelper
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_profile.users_pet_list
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 
 class UserProfileFragment(val user: User?) : Fragment() {
@@ -34,9 +46,8 @@ class UserProfileFragment(val user: User?) : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         Log.d(TAG, "username > ${user?.userName}")
         model = ViewModelProvider(this).get(TrixiViewModel::class.java)
-        getPosts()
-        getPets()
 
+        populateProfile()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -47,20 +58,72 @@ class UserProfileFragment(val user: User?) : Fragment() {
         super.onCreateOptionsMenu(menu, inflater)
     }
 
+    private fun populateProfile() {
+        profile_name.text = user!!.userName
+        profile_bio.text = user.bio
+        Picasso.get().load(BASE_URL + user.imageUrl).centerCrop().fit()
+            .into(user_profile_pet_image)
+        profile_following.text =
+            "Following " + (user.followingsPet?.size?.plus(user.followingsUser!!.size)).toString()
+        profile_followers.text = user.followers?.size.toString() + " Followers"
+
+        owner_name.visibility = View.INVISIBLE
+        follow_button.visibility = View.INVISIBLE
+
+        getPets()
+        getPosts()
+    }
+
     private fun getPosts() {
+
+        val snapHelper1: SnapHelper = GravitySnapHelper(Gravity.TOP)
+        snapHelper1.attachToRecyclerView(media_grid)
+
         user?.uid?.let {
             model.getPostsByOwner(it)?.observe(viewLifecycleOwner, Observer { posts ->
                 Log.d(TAG, "size: posts : ${posts?.size}")
+
+                media_grid.apply {
+
+                    //set up post thumbnails for user or show text:"no posts"
+                    if (!posts?.isEmpty()!!) {
+                        media_grid.layoutManager = GridLayoutManager(
+                            context,
+                            3,
+                            GridLayoutManager.VERTICAL,
+                            false
+                        )
+                        media_grid.adapter = ProfileMediaGridAdapter(posts as ArrayList<Post>)
+                    } else profile_no_posts.visibility = TextView.VISIBLE
+                }
             })
         }
     }
 
     private fun getPets() {
+        val snapHelper2: SnapHelper = GravitySnapHelper(Gravity.START)
+        snapHelper2.attachToRecyclerView(users_pet_list)
+
         user?.uid?.let {
             model.getPetsByOwner(it)?.observe(viewLifecycleOwner, Observer { pets ->
                 Log.d(TAG, "size: pets  : ${pets?.size}")
+
+                users_pet_list.apply {
+                    if (!pets?.isEmpty()!!) {
+                        //set pet list for user if not empty
+                        users_pet_list.layoutManager = GridLayoutManager(
+                            context,
+                            1,
+                            GridLayoutManager.HORIZONTAL,
+                            false
+                        )
+                        adapter = ProfilePetListAdapter(pets as ArrayList<Pet>)
+                        users_pet_list.adapter = adapter
+                    }
+                }
             })
         }
     }
+
 
 }
