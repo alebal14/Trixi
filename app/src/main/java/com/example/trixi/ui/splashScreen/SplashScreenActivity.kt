fc9c25fad21f7fbf8d007f8c8555cec7ba2652b7
Sofia <sofia.fredmans@gmail.com>
2021-01-05 10:25:33 +0100

    wip

--- app/src/main/java/com/example/trixi/ui/splashScreen/SplashScreenActivity.kt
+++ app/src/main/java/com/example/trixi/ui/splashScreen/SplashScreenActivity.kt
@@ -4,27 +4,25 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.PersistableBundle
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.trixi.R
 import com.example.trixi.ui.login.LoginActivity
 
-class Splash_screen_activity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT = 3000L
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_splash_screen)
 
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
 
-        Handler().postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
+        Handler().postDelayed(
+            {
+                val i = Intent(this, LoginActivity::class.java)
+                startActivity(i)
+                finish()
+            }, SPLASH_TIME_OUT)
+
     }
 
 }
